9dc610e69d4f5da0a43979e8b7b73a4a80894c7f
online-code-bite-md-tanvir-mahta <tmahtab57@gmail.com>
2021-10-22 09:47:27 +0600

    this is one kind of project

--- app/src/main/java/com/tanvircodder/taskclander/LifeArea.java
+++ app/src/main/java/com/tanvircodder/taskclander/LifeArea.java
@@ -1,4 +1,14 @@
 package com.tanvircodder.taskclander;
 
-public class LifeArea {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LifeArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_area);
+    }
 }
