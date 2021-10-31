b0b28f4141697915551fde385963e9e0f1d68ed8
online-code-bite-md-tanvir-mahta <tmahtab57@gmail.com>
2021-10-31 17:37:15 +0600

    this is one kind of project

--- app/src/main/java/com/tanvircodder/taskclander/AddActivity.java
+++ app/src/main/java/com/tanvircodder/taskclander/AddActivity.java
@@ -1,5 +1,6 @@
 package com.tanvircodder.taskclander;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,9 +9,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
 import com.tanvircodder.taskclander.model.Event;
 
 public class AddActivity extends AppCompatActivity {
@@ -19,6 +23,7 @@ public class AddActivity extends AppCompatActivity {
     private EditText mNameOfLocation;
     private Button mButton;
     private DatabaseReference mDatabase;
+    private FirebaseDatabase mFDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,6 @@ public class AddActivity extends AppCompatActivity {
 
         mNameOfEvent =(EditText) findViewById(R.id.event_name);
         mNameOfLocation = (EditText) findViewById(R.id.where);
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mButton  = (Button) findViewById(R.id.go_view);
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +40,7 @@ public class AddActivity extends AppCompatActivity {
                 String event_name = mNameOfEvent.getText().toString();
                 String event_place = mNameOfLocation.getText().toString();
                 Event event = new Event(event_name,event_place);
-                mDatabase.child(AddActivity.class.getSimpleName()).child(Event.class.getSimpleName())
+                mDatabase.child(AddActivity.class.getSimpleName()).child(event_name)
                         .setValue(event);
                 Toast.makeText(AddActivity.this,"The data has been saved",Toast.LENGTH_LONG)
                         .show();
--- app/src/main/java/com/tanvircodder/taskclander/ShowEventActivity.java
+++ app/src/main/java/com/tanvircodder/taskclander/ShowEventActivity.java
@@ -3,12 +3,20 @@ package com.tanvircodder.taskclander;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class ShowEventActivity extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
+    private TextView mEventName, mPlaceName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_event);
+
+        mEventName = (TextView) findViewById(R.id.name_event_view);
+        mPlaceName = (TextView) findViewById(R.id.place_of_the_event);
     }
+
 }
